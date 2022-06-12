633aa214c50c4dedacbb1a56afa8e9831477f88d
priyanshrm <priyanshmahendra@gmail.com>
2022-06-13 02:36:11 +0530

    CORS policy setup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.routers import post, user, vote, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]  # wildcard "every single domain or website"
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(vote.router)
