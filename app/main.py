0fea2996dfbe45164519455f145b08ce88b2cff5
priyanshrm <priyanshmahendra@gmail.com>
2022-06-12 18:13:42 +0530

    Initial Commit

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routers import post, user, vote, auth
+
+app = FastAPI()
+
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(vote.router)
+app.include_router(auth.router)
+
+@app.get('/')
+def root():
+    return {"message":"Hello, world!"}
